b5660e0bb246c5aae6d4085784c2dc913d1e134b
Greg Hurlman <greg@greghurlman.com>
2017-06-06 14:29:22 -0700

    Underps the derpitude

---
+++ Migrations/20170606171510_AddPeople.Designer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using dotnet_new_ember;
+
+namespace dotnetnewember.Migrations
+{
+    [DbContext(typeof(AppDbContext))]
+    [Migration("20170606171510_AddPeople")]
+    partial class AddPeople
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("dotnet_new_ember.Person", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("People");
+                });
+        }
+    }
+}
